d5a55d07ea08e3346b4350b1770696f6efeb81f9
wbwilsoniv <wbwilsoniv@gmail.com>
2018-08-03 15:09:36 -0400

    WIP CRUD functions added to models/players.js. models directory and models/players.js file created

---
+++ models/players.js
@@ -0,0 +1,46 @@
+const db = require('../connection');
+
+module.exports = {
+    findAll() {
+        return db.many(`
+        SELECT * 
+        FROM players
+        `);
+    },
+    
+    findOne(id) {
+        return db.one(`
+        SELECT *
+        FROM players
+        WHERE id = $1
+        `, id);
+    },
+
+    create(playerData) {
+        return db.none(`
+        INSERT INTO players
+        (name, team)
+        VALUES
+        ($1, $2)
+        RETURNING *
+        `, [playerData.name, playerData.team]);
+    },
+
+    delete(id) {
+        return db.one(`
+        DELETE FROM players
+        WHERE id = $1
+        `, id);
+    },
+
+    update(id, playerData) {
+        return db.one(`
+        UPDATE players
+        SET
+          name = $1,
+          team = $3
+        WHERE id = $1
+        RETURNING *
+        `, [id, playerData.name, playerData.team]);
+    },
+};
