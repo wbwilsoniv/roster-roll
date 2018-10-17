8a4ce41fcc1e27d5e945d30238e91149b7ee93bb
wbwilsoniv <wbwilsoniv@gmail.com>
2018-10-17 14:10:30 -0400

    moved link tags from ejs views into client/index.html. Added Home component and imported into app.jsx

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <section class="hero is-large">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Roster-Roll</h1>
+          <p class="subtitle">2.0</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
